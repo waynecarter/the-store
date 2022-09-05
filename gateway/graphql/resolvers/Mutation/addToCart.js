5806626de94fc165eaa87c8569a284f7316b019d
waynecarter <wayneacarter@hotmail.com>
2022-09-05 16:38:25 -0400

    Remove hack for numeric values.

--- gateway/graphql/resolvers/Mutation/addToCart.js
+++ gateway/graphql/resolvers/Mutation/addToCart.js
@@ -7,9 +7,7 @@ function addToCart(context, args, parent, info) {
     var cart = context.user.defaultCollection.get(cartId) || { items: {} };
     if (!cart.items) { cart.items = {} };
 
-    // HACK: cart.items[productId] returns a string for it's numberic value,
-    // so construct a Number from the value.
-    var count = Number(cart.items[productId]) || 0;
+    var count = cart.items[productId] || 0;
     cart.items[productId] = count + 1;
     
     context.user.defaultCollection.save(cartId, cart);
